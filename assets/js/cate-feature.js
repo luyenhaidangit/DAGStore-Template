3be8ad88d43e83f52859d923695e8bae5c19bdd5
luyenhaidangit <luyenhaidangit@gmail.com>
2022-08-13 23:58:20 +0700

    Xây dựng phần Cate-feature (Chưa hỗ trợ reponsive)

---
+++ assets/js/cate-feature.js
@@ -0,0 +1,11 @@
+var swiperCateFeature = new Swiper(".cate-feature__body", {
+        slidesPerView: 10,
+        grid: {
+          rows: 2,
+        },
+        // spaceBetween: 30,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+      });
